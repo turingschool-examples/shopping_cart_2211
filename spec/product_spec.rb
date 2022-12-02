81d1a14fcd2a1e881ee5a470d6200e799ebd36ab
Dani Coleman <danicoleman00@gmail.com>
2022-12-02 13:11:40 -0700

    create iteration 1 tests

---
+++ spec/product_spec.rb
@@ -0,0 +1,33 @@
+require './lib/product'
+
+RSpec.describe Product do
+  describe 'Iteration 1' do
+    it 'exists' do
+      product = Product.new(:paper, 'toilet paper', 3.70, '10')
+
+      expect(product).to be_a(Product)
+    end
+
+    xit 'has attributes' do 
+      product = Product.new(:paper, 'toilet paper', 3.70, '10')
+
+      expect(product.category).to eq(:paper)
+      expect(product.name).to eq('toilet paper')
+      expect(product.unit_price).to eq(3.70)
+      expect(product.quantity).to eq(10)
+    end
+    
+    xit 'starts out as not hoarded' do
+      product = Product.new(:paper, 'toilet paper', 3.70, '10')
+
+      expect(product.is_hoarded?).to eq(false)
+    end
+
+    xit 'can be hoarded' do
+      product = Product.new(:paper, 'toilet paper', 3.70, '10')
+
+      product.hoard
+      expect(product.is_hoarded?).to eq(true)
+    end
+  end
+end
