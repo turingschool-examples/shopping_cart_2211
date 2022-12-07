e392c3a880bc2c8b2a2e5f65b4d70807b01f7ff3
Matt Enyeart <menyeart1@gmail.com>
2022-12-07 15:54:24 -0700

    Add a test for the sorted products by quantity method

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -97,6 +97,19 @@ RSpec.describe ShoppingCart do
       expect(cart.percentage_occupied).to eq(43.33)
     end
 
+    it "can sort products by quantity" do
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    product4 = Product.new(:produce, 'apples', 0.99, '20')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    cart.add_product(product4)
+    expect(cart.sorted_products_by_quantity).to eq([product3, product2, product1, product4])
+    end
+
 
 
 
