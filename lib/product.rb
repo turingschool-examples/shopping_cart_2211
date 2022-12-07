bdb8ee689c426d656d8719b5651c161f45d89ccb
Elle Majors <hellolovely@ellemajors>
2022-12-07 16:36:20 -0700

    feat: can be hoarded #is_hourded? and #hourd

--- lib/product.rb
+++ lib/product.rb
@@ -3,11 +3,23 @@ class Product
               :name, 
               :unit_price, 
               :quantity
-              
+              # :is_hoarded?
+
   def initialize(category, name, unit_price, quantity)
     @category = category
     @name = name
     @unit_price = unit_price
-    @quantity = quantity
+    @quantity = quantity.to_i
+    @is_hoarded = false
+  end
+
+  def is_hoarded?
+    @is_hoarded
+  end
+
+  def hoard
+    if !@is_hoarded 
+      @is_hoarded = true
+    end
   end
 end
