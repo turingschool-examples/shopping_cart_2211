2ca5cb13d1a418c9590c790e110b7bc96552c3cc
Matt Enyeart <menyeart1@gmail.com>
2022-12-07 15:17:06 -0700

    Add another test for the is full method

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -57,6 +57,22 @@ RSpec.describe ShoppingCart do
       expect(cart.total_number_of_products).to eq(13)
     end
 
+    it "can be full when number of products are greater than capacity" do
+      cart = ShoppingCart.new("King Soopers", "30items")
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')
+      product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+      product4 = Product.new(:produce, 'apples', 0.99, '20')
+      cart.add_product(product1)
+      cart.add_product(product2)
+      cart.add_product(product3)
+      cart.add_product(product4)
+      expect(cart.total_number_of_products).to eq(33)
+      expect(cart.is_full?).to eq(true)
+
+    end
+
+
 
 
 
