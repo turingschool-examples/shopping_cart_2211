454d62c4d2d01904973bc2dd1241325b28cb3c32
Brian Hayes <Brian.hayes1200@gmail.com>
2022-12-09 13:46:57 -0700

    Test: add test for total number of products method for class shopping cart

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -38,5 +38,17 @@ RSpec.describe ShoppingCart do
 
       expect(cart.details).to eq({name: 'King Soopers', capacity: 30})
     end
+
+    it 'will list total number of products in shopping cart' do 
+      cart = ShoppingCart.new("King Soopers", "30items")
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2') 
+      product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+      cart.add_product(product1)
+      cart.add_product(product2)
+      cart.add_product(product3)
+
+      expect(cart.total_number_of_products).to eq(13)
+    end
   end
 end
