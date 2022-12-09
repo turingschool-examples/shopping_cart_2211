d1298c7d512fda555c9b75cc6223ecc4658aa9ee
Brian Hayes <Brian.hayes1200@gmail.com>
2022-12-09 13:33:00 -0700

    Feat: add hoard and is hoarded? methods for class product

--- lib/product.rb
+++ lib/product.rb
@@ -2,12 +2,21 @@ class Product
   attr_reader :category,
               :name,
               :unit_price, 
-              :quantitiy
+              :quantity
 
   def initialize(category, name, unit_price, quantity)
     @category = category
     @name = name 
     @unit_price = unit_price 
-    @quantity = quantity
+    @quantity = quantity.to_i
+    @hoard = false
+  end
+
+  def hoard 
+    @hoard == true
+  end
+  
+  def is_hoarded?
+    @hoard
   end
 end
