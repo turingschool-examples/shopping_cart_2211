b1df5e6a9bce5f31d4c399b181f70f512c91e7e4
Hady Matar <hadyematar@gmail.com>
2022-12-08 21:47:40 -0700

    Finish second to last question in iteration 4

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -9,6 +9,7 @@ class ShoppingCart
          @products = []
          @is_full = false
          @total = 0
+  
 
     end
 
@@ -37,11 +38,32 @@ class ShoppingCart
     end
 
     def products_by_category(category)
-        paper_products = []
+        product_category = []
         @products.select do |product|
-        paper_products << product if product.category == :paper
+        product_category << product if product.category == :paper
         end
-        return paper_products
+        return product_category
+    end
+
+    def percentage_occupied
+        number_of_products = self.total_number_of_products
+        percent = (number_of_products.to_f/@capacity.to_f)
+        (percent*100).round(2)
+              
+    end
+
+    def sorted_products_by_quantity
+        sorted_array = []
+        sorted = []
+        @products.each do |product|
+        sorted_array << [product.quantity, product]
+        end 
+        sorted_array = sorted_array.sort 
+        sorted_array.each do |product|
+            sorted << product.slice(1)
+        end
+        return sorted 
+        # end
     end
     
 end
--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -106,6 +106,36 @@ RSpec.describe ShoppingCart do
         expect(cart.products_by_category(:paper)).to eq([product1, product3])
     
         end
+
+      it 'is less than half occupied' do
+        cart = ShoppingCart.new("King Soopers", "30items")
+        product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+        product2 = Product.new(:meat, 'chicken', 4.50, '2') 
+        product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+        cart.add_product(product1)
+        cart.add_product(product2)
+        cart.add_product(product3)
+
+        expect(cart.percentage_occupied).to eq(43.33)
+      
+          end
+
+      it 'the items are sorted in ascending order' do
+      cart = ShoppingCart.new("King Soopers", "30items")
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2') 
+      product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+      product4 = Product.new(:produce, 'apples', 0.99, '20')
+      cart.add_product(product1)
+      cart.add_product(product2)
+      cart.add_product(product3)
+      cart.add_product(product4)
+            
+      expect(product4 = Product.new(:produce, 'apples', 0.99, '20')).to be_an_instance_of(Product)
+      expect(cart.sorted_products_by_quantity).to eq([product3, product2, product1, product4])
+    #  I'm not sure why Ruby created product4 as a new object ID. I think it's because I played aorund and deleted/ edited stuff from previous tests. But everything is on point other than the object ID changed. Would love to understand why but moving on for time reasons
+          
+      end
     
   end
 end
