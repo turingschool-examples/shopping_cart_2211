06db1b98aa6a3e7205c11e3fd2ab73e0130aa1b6
Hady Matar <hadyematar@gmail.com>
2022-12-08 20:13:36 -0700

    Complete iteration 2

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -3,26 +3,26 @@ require './lib/shopping_cart'
 
 RSpec.describe ShoppingCart do
   describe 'Iteration 2' do
-    xit 'exists' do
+    it 'exists' do
       cart = ShoppingCart.new("King Soopers", "30items")
 
       expect(cart).to be_a(ShoppingCart)
     end
 
-    xit 'has attributes' do
+    it 'has attributes' do
       cart = ShoppingCart.new("King Soopers", "30items")
 
       expect(cart.name).to eq("King Soopers")
       expect(cart.capacity).to eq(30)
     end
 
-    xit 'starts with no products' do
+    it 'starts with no products' do
       cart = ShoppingCart.new("King Soopers", "30items")
 
       expect(cart.products).to eq([])
     end
 
-    xit 'can add products to the cart' do
+    it 'can add products to the cart' do
       cart = ShoppingCart.new("King Soopers", "30items")
       product_1 = Product.new(:paper, 'toilet paper', 3.70, '10')
       product_2 = Product.new(:meat, 'chicken', 4.50, '2')
@@ -33,9 +33,9 @@ RSpec.describe ShoppingCart do
       expect(cart.products).to eq([product_1, product_2])
     end
 
-    xit 'creates a hash of cart details' do
+    it 'creates a hash of cart details' do
       cart = ShoppingCart.new("King Soopers", "30items")
-
+      
       expect(cart.details).to eq({name: 'King Soopers', capacity: 30})
     end
   end
