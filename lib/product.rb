89a09cc0d795ee0334e8ec281c8fb694160ed7c9
Brian Hayes <Brian.hayes1200@gmail.com>
2022-12-09 13:29:27 -0700

    Feat: add initialize method for class product

--- lib/product.rb
+++ lib/product.rb
@@ -1,2 +1,13 @@
 class Product
+  attr_reader :category,
+              :name,
+              :unit_price, 
+              :quantitiy
+
+  def initialize(category, name, unit_price, quantity)
+    @category = category
+    @name = name 
+    @unit_price = unit_price 
+    @quantity = quantity
+  end
 end
