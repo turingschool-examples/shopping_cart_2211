6e8e2ee0b1c8946e3f3f88985c588951cbf4d5c0
Brian Hayes <Brian.hayes1200@gmail.com>
2022-12-09 13:39:12 -0700

    Feat: add add products method for class shopping cart

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,13 +1,18 @@
 class ShoppingCart
-  attr_reader :name
+  attr_reader :name, :products
 
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
+    @products = []
   end
 
   def capacity
     @capacity.delete "items"
     @capacity.to_i
+  end
+
+  def add_product(product)
+    @products << product
   end
 end
