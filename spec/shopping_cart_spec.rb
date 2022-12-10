92acb95cbc1e55206f5d0970764477db3a084719
KaraJH <karas.jones@yahoo.com>
2022-12-10 15:24:10 -0700

    test: Sorted products by quantity test

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -116,4 +116,23 @@ RSpec.describe ShoppingCart do
       expect(cart.percentage_occupied).to eq(43.33)
     end
   end
+
+  describe '#sorted_products_by_quantity' do 
+    it 'sorts the products by quantity from the largest quantity to the least' do 
+      cart = ShoppingCart.new("King Soopers", "30items")
+
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')  
+      product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+      product4 = Product.new(:produce, 'apples', 0.99, '20') 
+
+      cart.add_product(product1)
+      cart.add_product(product2)
+      cart.add_product(product3)
+      cart.add_product(product4)
+
+      expect(cart.sorted_products_by_quantity).to eq([product4, product1, product2, product3])
+
+    end
+  end
 end
