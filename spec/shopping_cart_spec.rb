caa092514e22b480e18407e5c41d43ab86042153
KaraJH <karas.jones@yahoo.com>
2022-12-10 15:13:56 -0700

    test: Test for products by category method

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -65,12 +65,38 @@ RSpec.describe ShoppingCart do
       product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
 
       cart.add_product(product1)
-      cart.add_product(product1)
-      cart.add_product(product1)
+      cart.add_product(product2)
+      cart.add_product(product3)
 
       cart.total_number_of_products
 
       expect(cart.is_full?).to eq(false)
+
+      product4 = Product.new(:produce, 'apples', 0.99, '20') 
+
+      cart.add_product(product4) 
+
+      expect(cart.is_full?).to eq(true)
+    end
+  end
+
+  describe '#products_by_category' do 
+    it 'returns the products associated with the given category' do
+      cart = ShoppingCart.new("King Soopers", "30items")
+
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')  
+      product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+      product4 = Product.new(:produce, 'apples', 0.99, '20') 
+
+      cart.add_product(product1)
+      cart.add_product(product2)
+      cart.add_product(product3)
+      cart.add_product(product4)
+
+      expect(cart.products_by_category(:paper)).to eq([product1, product3])
+      expect(cart.products_by_category(:meat)).to eq([product2])
+      expect(cart.products_by_category(:produce)).to eq([product4])
     end
   end
 end
