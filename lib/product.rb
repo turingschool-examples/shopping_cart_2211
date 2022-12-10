5becf683c06d4717af2e9d3080a114be95fe726a
Huy Phan <huyphan520@gmail.com>
2022-12-10 10:39:37 -0600

    Iteration 1 complete

--- lib/product.rb
+++ lib/product.rb
@@ -1,2 +1,23 @@
 class Product
+    attr_reader :category, :name, :unit_price, :quantity
+    def initialize(category, name, unit_price, quantity)
+        @category = category
+        @name = name
+        @unit_price = unit_price
+        @quantity = quantity
+        @is_hoarded = false
+    end
+
+    def quantity
+        @quantity.to_i
+    end
+
+    def is_hoarded?
+        @is_hoarded
+    end
+
+    def hoard
+        @is_hoarded = true
+    end
+  
 end
