a4ebd969e401c1abb427ebefb7c7ff20eb56b988
KaraJH <karas.jones@yahoo.com>
2022-12-10 15:32:54 -0700

    test: product breakdown method test

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -132,7 +132,30 @@ RSpec.describe ShoppingCart do
       cart.add_product(product4)
 
       expect(cart.sorted_products_by_quantity).to eq([product4, product1, product2, product3])
+    end
+  end
 
+  describe '#product_breakdown' do 
+    it 'is a hash of products, sorted by their categories' do 
+      cart = ShoppingCart.new("King Soopers", "30items")
+
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')  
+      product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+      product4 = Product.new(:produce, 'apples', 0.99, '20') 
+
+      cart.add_product(product1)
+      cart.add_product(product2)
+      cart.add_product(product3)
+      cart.add_product(product4)
+
+      expected = {
+                    meat: [product2],
+                    paper: [product1, product3],
+                    produce: [product4]
+      }
+
+      expect(cart.product_breakdown).to eq(expected)
     end
   end
 end
