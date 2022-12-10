8171024e131169dee8e1f1f20b1790265f55fe1d
David Marino <davidjmarino8@gmail.com>
2022-12-10 18:42:10 -0500

    Initial commit

--- lib/product.rb
+++ lib/product.rb
@@ -1,2 +1,17 @@
 class Product
+    attr_reader :category,
+                :name,
+                :unit_price,
+                :quantity
+                
+
+    def initialize(category, name, unit_price, quantity)
+        @category = category
+        @name = name
+        @unit_price = unit_price
+        @quantity = quantity.to_i
+        
+    end
+    
+    
 end
