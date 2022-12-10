a9d21c257167f968d407559fc9207803c827c6b3
KaraJH <karas.jones@yahoo.com>
2022-12-10 14:54:40 -0700

    feat: Adds attributes to the shopping cart class

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,3 +1,12 @@
 class ShoppingCart
-  
+  attr_reader :name, :capacity, :products
+
+  def initialize(name, capacity)
+    @name = name 
+    @capacity = capacity.delete("items").to_i
+    @products = []
+  end
+
+ 
+
 end
