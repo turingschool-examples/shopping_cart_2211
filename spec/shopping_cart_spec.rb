cd19b105592b702d29942e7f308f46e3cf3befd7
Adam Bailey <ab67319@gmail.com>
2022-12-10 14:49:50 -0700

    Create 'Iteration 3' describe pattern with before block

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -39,4 +39,14 @@ RSpec.describe ShoppingCart do
       expect(cart.details).to eq({name: 'King Soopers', capacity: 30})
     end
   end
+
+  describe 'Iteration 3' do
+    before(:each) do
+      @cart = ShoppingCart.new("King Soopers", "30items")
+      @product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      @product2 = Product.new(:meat, 'chicken', 4.50, '2')
+      @product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+      @product4 = Product.new(:produce, 'apples', 0.99, '20')
+    end
+  end
 end
