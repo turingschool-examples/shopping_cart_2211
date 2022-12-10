8e2954d51ad8cef77d8620a8db7d7412ef4d5b1d
KaraJH <karas.jones@yahoo.com>
2022-12-10 15:29:43 -0700

    feat: Sorted by quantity method

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -41,4 +41,10 @@ class ShoppingCart
     ((total_number_of_products / @capacity.to_f) * 100).round(2)
   end
 
+  def sorted_products_by_quantity 
+    @products.sort_by do |product|
+      product.quantity
+    end.reverse
+  end
+
 end
