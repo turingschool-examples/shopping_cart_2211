bc9b3bdd0fec577942e9458d18c942d97d201a4f
Caleb Thomas <cjthomas00@comcast.net>
2022-12-10 12:45:27 -0700

    feat: added #details.

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,8 +1,18 @@
 class ShoppingCart
-  attr_reader :name, :capacity, :products
+  attr_reader :name, :capacity, :products, :details
   def initialize(name, capacity)
     @name = name
     @capacity = 30
     @products = []
+  end
+
+  def add_product(product)
+    @products << product
+  end
+
+  def details
+    {capacity: @capacity,
+     name: @name
+    }
   end
 end
