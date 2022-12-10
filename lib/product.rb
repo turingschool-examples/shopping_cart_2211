c7c8f541963605a3b47b96069726b4e4d777bc80
David Marino <davidjmarino8@gmail.com>
2022-12-10 18:43:18 -0500

    feat: starts out as not hoarded

--- lib/product.rb
+++ lib/product.rb
@@ -2,16 +2,19 @@ class Product
     attr_reader :category,
                 :name,
                 :unit_price,
-                :quantity
-                
+                :quantity,
+                :is_hoarded
 
     def initialize(category, name, unit_price, quantity)
         @category = category
         @name = name
         @unit_price = unit_price
         @quantity = quantity.to_i
+        @is_hoarded = false
         
     end
     
-    
+    def is_hoarded?
+        is_hoarded
+    end
 end
