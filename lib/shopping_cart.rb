988cc5681e4655ba2a3e91a78cd558ecb2c73c40
Jade Stewart <jadekstewart3@gmail.com>
2022-12-10 11:00:05 -0700

    Create class ShoppingCart and pass first four iteration 2 tests

---
+++ lib/shopping_cart.rb
@@ -0,0 +1,16 @@
+class ShoppingCart
+    attr_reader :name,
+                :capacity,
+                :products
+
+    def initialize(name, capacity)
+        @name = name
+        @capacity = capacity.to_i
+        @products = []
+    end
+
+    def add_product(item)
+        @products << item
+    end
+
+end
