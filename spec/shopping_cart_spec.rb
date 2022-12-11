c6b17049a4f2afbba9bab10cf2674463aae21da5
David Marino <davidjmarino8@gmail.com>
2022-12-10 19:01:45 -0500

    debug last error and complete 3 tests

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -3,20 +3,20 @@ require './lib/shopping_cart'
 
 RSpec.describe ShoppingCart do
   describe 'Iteration 2' do
-    xit 'exists' do
+    it 'exists' do
       cart = ShoppingCart.new("King Soopers", "30items")
 
       expect(cart).to be_a(ShoppingCart)
     end
 
-    xit 'has attributes' do
+    it 'has attributes' do
       cart = ShoppingCart.new("King Soopers", "30items")
 
       expect(cart.name).to eq("King Soopers")
       expect(cart.capacity).to eq(30)
     end
 
-    xit 'starts with no products' do
+    it 'starts with no products' do
       cart = ShoppingCart.new("King Soopers", "30items")
 
       expect(cart.products).to eq([])
