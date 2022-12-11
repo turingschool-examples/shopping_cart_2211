b2b33d117cc30b9ecd976f51ef15483cefdad8a6
Brad Dunlap <braddunlapdpt@gmail.com>
2022-12-11 16:41:48 -0700

    Finish iteration 1

--- lib/product.rb
+++ lib/product.rb
@@ -1,2 +1,26 @@
 class Product
+attr_reader :category,
+						:name,
+						:unit_price,
+						:quantity,
+						:is_hoarded
+	def initialize(category, name, unit_price, quantity)
+		@category = category
+		@name = name
+		@unit_price = unit_price
+		@quantity = quantity.to_i
+		@is_hoarded = false
+	end
+
+	def is_hoarded?
+		if is_hoarded == true
+			true
+		else
+			false
+		end
+	end
+
+	def hoard
+		@is_hoarded = true
+	end
 end
