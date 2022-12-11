8e33a17f9a2abe60ef3b2463e8b278b4eddb0a30
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-12-11 09:52:54 -0800

    Feat: create #products_by_category(category) method and test

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -31,4 +31,10 @@ class ShoppingCart
   def is_full?
     total_number_of_products > capacity
   end 
+
+  def products_by_category(category)
+    products.select do |product|
+      product.category == category
+    end 
+  end 
 end
--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -43,12 +43,12 @@ RSpec.describe ShoppingCart do
   describe 'Iteration 3' do
     it 'counts the total number of products' do
       cart = ShoppingCart.new("King Soopers", "30items")
-      product_1 = Product.new(:paper, 'toilet paper', 3.70, '10')
-      product_2 = Product.new(:meat, 'chicken', 4.50, '2')
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')
       product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
 
-      cart.add_product(product_1)
-      cart.add_product(product_2)
+      cart.add_product(product1)
+      cart.add_product(product2)
       cart.add_product(product3)
 
       expect(cart.total_number_of_products).to eq(13)
@@ -56,12 +56,12 @@ RSpec.describe ShoppingCart do
 
     it 'is not full' do
       cart = ShoppingCart.new("King Soopers", "30items")
-      product_1 = Product.new(:paper, 'toilet paper', 3.70, '10')
-      product_2 = Product.new(:meat, 'chicken', 4.50, '2')
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')
       product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
 
-      cart.add_product(product_1)
-      cart.add_product(product_2)
+      cart.add_product(product1)
+      cart.add_product(product2)
       cart.add_product(product3)
 
       expect(cart.is_full?).to be false
@@ -69,17 +69,32 @@ RSpec.describe ShoppingCart do
 
     it 'is full' do
       cart = ShoppingCart.new("King Soopers", "30items")
-      product_1 = Product.new(:paper, 'toilet paper', 3.70, '10')
-      product_2 = Product.new(:meat, 'chicken', 4.50, '2')
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')
       product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
       product4 = Product.new(:produce, 'apples', 0.99, '20')
 
-      cart.add_product(product_1)
-      cart.add_product(product_2)
+      cart.add_product(product1)
+      cart.add_product(product2)
       cart.add_product(product3)
       cart.add_product(product4)
 
       expect(cart.is_full?).to be true
     end 
+
+    it 'finds products by category' do
+      cart = ShoppingCart.new("King Soopers", "30items")
+      product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+      product2 = Product.new(:meat, 'chicken', 4.50, '2')
+      product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+      product4 = Product.new(:produce, 'apples', 0.99, '20')
+
+      cart.add_product(product1)
+      cart.add_product(product2)
+      cart.add_product(product3)
+      cart.add_product(product4)
+
+      expect(cart.products_by_category(:paper)).to eq([product1, product3])
+    end
   end
 end
