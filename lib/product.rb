7c88f81286f2a57de5a1601d38fbc7160d8125af
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-12-11 13:08:10 -0700

    Add product tests and methods

--- lib/product.rb
+++ lib/product.rb
@@ -1,2 +1,31 @@
 class Product
+    attr_reader :category, 
+                :name, 
+                :unit_price, 
+                :quantity,
+                :is_hoarded
+
+    def initialize(category, name, unit_price, quantity)
+        @category = category
+        @name = name
+        @unit_price = unit_price
+        @quantity = quantity
+        @is_hoarded = false
+    end
+
+    def quantity
+        @quantity.to_i
+    end
+
+    def total_price
+        @unit_price * @quantity.to_f
+    end
+
+    def is_hoarded?
+        @is_hoarded
+    end
+
+    def hoard
+        @is_hoarded = true
+    end
 end
