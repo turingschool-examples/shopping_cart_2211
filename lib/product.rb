a93ccc9d92fa6fd42d38f43a3f271b71135661ad
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-12-11 09:12:15 -0800

    Feat: create #is_hoarded? and #hoard methods to pass last 2 tests

--- lib/product.rb
+++ lib/product.rb
@@ -2,12 +2,22 @@ class Product
   attr_reader :category,
               :name,
               :unit_price,
-              :quantity
+              :quantity,
+              :hoard
   
   def initialize(category, name, unit_price, quantity)
     @category = category
     @name = name
     @unit_price = unit_price
     @quantity = quantity.to_i
+    @hoard = 0
+  end
+
+  def is_hoarded?
+    @hoard != 0
+  end
+
+  def hoard
+    @hoard += 1
   end
 end
