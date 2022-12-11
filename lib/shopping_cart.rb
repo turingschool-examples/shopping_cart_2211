ff7e976e90d64952e72b52adefef32c3c7da538b
Bobby Luly <bobbyluly@gmail.com>
2022-12-11 16:17:20 -0700

    Feat- add products_by_category method

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -31,4 +31,10 @@ class ShoppingCart
         total_number_of_products >= @capacity
     end
 
+    def products_by_category(category)
+            @products.find_all do |product|
+                product.category == category
+            end
+    end
+
 end
