891f8e3beaee7c321a009e3464358b1863c3d597
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-12-12 16:43:49 -0700

    Define attributes, and #capacity - pass test 2

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,7 +1,12 @@
 class ShoppingCart
+attr_reader :name, :capacity
 
-def initialize(name, capacity)
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity
+  end
 
-end
-
+  def capacity
+    @capacity.sub('items', '').to_i
+  end
 end
