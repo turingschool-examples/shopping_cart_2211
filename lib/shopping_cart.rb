2e67c71c1986832281491be97191d7da69900602
Tori Enyart <torienyart@gmail.com>
2022-12-12 09:26:35 -0700

    feat: Create method to spit out cart details as hash

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -9,4 +9,11 @@ class ShoppingCart
     def add_product(product)
         @products << product
     end
+
+    def details
+        details = {
+            name: @name,
+            capacity: @capacity
+        }
+    end
 end
