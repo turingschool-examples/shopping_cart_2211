8889228078401f68cc0bb7453a964d1af9079bea
Chris Crane <chrisjcrane@gmail.com>
2022-12-12 07:38:05 -0700

    complete 3 tests in product class

--- lib/product.rb
+++ lib/product.rb
@@ -1,2 +1,20 @@
 class Product
+    attr_reader :category,
+                :name,
+                :unit_price,
+                :quantity,
+                :is_hoarded
+
+    def initialize(category, name, unit_price, quantity)
+        @category = category
+        @name = name.to_s
+        @unit_price = unit_price.to_f
+        @quantity = quantity.to_i
+        @is_hoarded = false
+    end
+
+    def is_hoarded?
+        @is_hoarded
+    end
+
 end
