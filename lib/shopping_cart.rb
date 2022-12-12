81f0acd10c94104742e0c75018bfccc340d60542
Chris Crane <chrisjcrane@gmail.com>
2022-12-12 07:56:52 -0700

    create hash of details for the cart

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,15 +1,21 @@
 class ShoppingCart
     attr_reader :name,
                 :capacity,
-                :products
+                :products,
+                :details
 
     def initialize(name, capacity)
         @name = name
         @capacity = capacity.to_i
         @products = []
+        @details = {
+            :name => @name,
+            :capacity => @capacity
+        }
     end
 
     def add_product(product)
         @products << product
     end
+
 end
