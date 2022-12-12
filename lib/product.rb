15ab0bdc072d5702cfe6c9929877b35db3084de3
Isaac Alter <i_alter@yahoo.com>
2022-12-12 11:01:25 -0700

    Initial commit

--- lib/product.rb
+++ lib/product.rb
@@ -1,2 +1,23 @@
 class Product
+    attr_reader :category,
+                :name,
+                :unit_price,
+                :quantity,
+                :hoard_check
+
+    def initialize(category, name, unit_price, quantity)
+        @category = category
+        @name = name
+        @unit_price = unit_price
+        @quantity = quantity.to_i
+        @hoard_check = false
+    end
+
+    def is_hoarded?
+        return @hoard_check
+    end
+
+    def hoard
+        @hoard_check = true
+    end
 end
