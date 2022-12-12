28cc4071270479b5d30da78d34f6cb07d9199275
Melony Franchini <meltravelz@gmail.com>
2022-12-12 13:23:03 -0700

    feat: Add initialize method passing in variables: category, name, price, quantity

--- lib/product.rb
+++ lib/product.rb
@@ -1,2 +1,13 @@
 class Product
+    attr_reader :category, 
+                :name, 
+                :price,
+                :quantity
+
+    def initialize(category, name, price, quantity)
+        @category = category
+        @name = name
+        @price = price
+        @quantity = quantity
+    end
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,16 +1,16 @@
+require 'rspec'
 require './lib/product'
 
 RSpec.describe Product do
-  describe 'Iteration 1' do
-    it 'exists' do
-      product = Product.new(:paper, 'toilet paper', 3.70, '10')
+  let(:product) {Product.new(:paper, 'toilet paper', 3.70, '10')}
 
+  describe 'Iteration 1' do
+
+    it 'exists' do
       expect(product).to be_a(Product)
     end
 
     xit 'has attributes' do 
-      product = Product.new(:paper, 'toilet paper', 3.70, '10')
-
       expect(product.category).to eq(:paper)
       expect(product.name).to eq('toilet paper')
       expect(product.unit_price).to eq(3.70)
@@ -18,14 +18,10 @@ RSpec.describe Product do
     end
     
     xit 'starts out as not hoarded' do
-      product = Product.new(:paper, 'toilet paper', 3.70, '10')
-
       expect(product.is_hoarded?).to eq(false)
     end
 
     xit 'can be hoarded' do
-      product = Product.new(:paper, 'toilet paper', 3.70, '10')
-
       product.hoard
       expect(product.is_hoarded?).to eq(true)
     end
