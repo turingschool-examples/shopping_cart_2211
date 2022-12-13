c20cccca8d13de89b4d33f5fe6cdcf91dfbb759e
Carol Rieping <carol.rieping@gmail.com>
2022-12-12 18:39:51 -0700

    Add Iter 1 test 1

--- lib/product.rb
+++ lib/product.rb
@@ -1,2 +1,15 @@
 class Product
+
+    attr :category,
+         :name,
+         :quantity,
+         :unit_price
+
+def initialize(category, name, quantity, unit_price)
+    @category = category
+    @name = name
+    @quantity = quantity
+    @unit_price = unit_price
+end
+
 end
