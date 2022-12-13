854a377c934149c55a70d96fc3dae484bb13ee7a
Weston Sandfort <sandfortw@gmail.com>
2022-12-13 14:36:05 -0700

    Add ShoppingCart class that passes the spec

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,10 +1,22 @@
 require 'product'
 
 class ShoppingCart
-attr_reader :name, :capacity
+attr_reader :name, :capacity, :products
 
   def initialize(name, capacity)
     @name = name
-    @capacity = capacity
+    @capacity = capacity.chop.chop.chop.chop.chop.to_i
+    @products = []
+  end
+
+  def add_product(product)
+    @products << product
+  end
+
+  def details
+    details_hash = {}
+    details_hash[:name] = @name
+    details_hash[:capacity] = @capacity
+    details_hash
   end
 end
