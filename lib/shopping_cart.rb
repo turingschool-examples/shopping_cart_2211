22bc33fbdea4b78f14bc6c8f0af4376698ec1285
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-12-12 17:17:45 -0700

    Correct #capacity, create #add_product - pass test 5

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -3,15 +3,23 @@ attr_reader :name, :capacity, :products
 
   def initialize(name, capacity)
     @name = name
-    @capacity = capacity
+    @capacity = capacity.to_i
     @products = []
+    @cart_details = {
+      name: @name,
+      capacity: @capacity,
+    }
   end
 
   def capacity
-    @capacity.sub('items', '').to_i
+    @capacity
   end
-
+require 'pry'; binding.pry
   def add_product(product)
     @products << product
+  end
+
+  def details
+    @cart_details
   end
 end
