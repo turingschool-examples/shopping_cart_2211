827ea4d1e0c894d9c1781c267610fe6cb5ac43d7
Conner Van Loan <cdvanloan@gmail.com>
2022-12-13 11:55:58 -0700

    Feat:shopping cart class added

---
+++ lib/shopping_cart.rb
@@ -0,0 +1,18 @@
+require './lib/product'
+
+class ShoppingCart
+  attr_reader :name, :capacity, :products
+  def initialize(name, capacity)
+    @name = name
+    @capacity = capacity.to_i
+    @products = []
+  end
+
+  def add_product(product)
+    @products << product
+  end
+
+  def details
+    
+  end
+end
