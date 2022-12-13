65555ea1dbc12cb96c4f6f402e64d2d4f2ebcdab
Carol Rieping <carol.rieping@gmail.com>
2022-12-12 19:12:40 -0700

    Add Iteration 1 tests

--- lib/product.rb
+++ lib/product.rb
@@ -1,15 +1,27 @@
 class Product
 
-    attr :category,
-         :name,
-         :quantity,
-         :unit_price
+  attr  :category,
+        :name,
+        :quantity,
+        :unit_price
 
-def initialize(category, name, quantity, unit_price)
+  def initialize(category, name, quantity, unit_price)
     @category = :paper
     @name = 'toilet paper'
     @quantity = 10
     @unit_price = 3.7
-end
+  end
+
+  def is_hoarded?
+    if hoard == true
+      true
+    else
+      false
+    end
+  end
+
+  def hoard
+    false
+  end
 
 end
