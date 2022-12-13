08c1371fb3ede75a9060056251cc57d275861c09
Danae Mata <danae.mata1515@gmail.com>
2022-12-13 10:34:11 -0500

    Add changes to product file

--- lib/product.rb
+++ lib/product.rb
@@ -1,2 +1,20 @@
 class Product
+    attr_reader :category, :name, :unit_price, :quantity
+    def initialize(category, name, unit_price, quantity)
+        @category = category
+        @name = name
+        @unit_price = unit_price
+        @quantity = quantity
+    end
+
+    def is_hoarded?
+       if category == true
+        true
+       else
+        false
+       end
+    end
+
+    def hoard
+    end
 end
