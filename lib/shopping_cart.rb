5cc59f38c5bdce35698d5498fcd821b65141bd21
Nickolas Marquez <nickomarquez7@gmail.com>
2022-12-13 16:05:32 -0700

    Empty array_products

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,12 +1,14 @@
 class ShoppingCart
     attr_reader :name, :capacity
-    
+
     def initialize(name, capacity)
         @name = "King Soopers"
         @capacity = capacity
     end 
 
-
+    def products
+        products = ([])
+    end
 
 
 
